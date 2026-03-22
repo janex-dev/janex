cc6b6cf8776ed4da87b3b0ae27bf42c81411645e
Glavo <zjx001202@gmail.com>
2026-03-22 15:03:06 +0800

    implement ClassFile parsing logic

--- janex-core/src/classfile.rs
+++ janex-core/src/classfile.rs
@@ -1,26 +1,120 @@
 // Copyright (c) 2026 Glavo
 // SPDX-License-Identifier: MPL-2.0
 
+/// A class file in the JVM class file format.
+///
+/// See [JVM Spec §4.1](https://docs.oracle.com/javase/specs/jvms/se25/html/jvms-4.html#jvms-4.1).
 pub struct ClassFile {
     pub minor_version: u16,
     pub major_version: u16,
     pub constant_pool_count: u16,
+    pub constant_pool: Box<[ConstantPoolInfo]>,
+    pub access_flags: u16,
+    pub this_class: u16,
+    pub super_class: u16,
+    pub interfaces: Box<[u16]>,
+    pub fields: Box<[u16]>,
+    pub methods: Box<[u16]>,
+    pub attributes: Box<[AttributeInfo]>,
 }
 
 impl ClassFile {
+    /// The magic number for a class file.
+    pub const MAGIC_NUMBER: u32 = 0xCAFEBABE;
+
+    /// Parses a class file from the given bytes.
     pub fn parse(bytes: &[u8]) -> Result<ClassFile, Error> {
         let mut parser = ClassFileParser { bytes };
+
+        let magic = parser.read_u32()?;
+        if magic != Self::MAGIC_NUMBER {
+            return Err(Error::InvalidMagicNumber(magic));
+        }
 
         let minor_version = parser.read_u16()?;
         let major_version = parser.read_u16()?;
 
         let constant_pool_count = parser.read_u16()?;
+        let constant_pool = Self::read_constant_pool(&mut parser, constant_pool_count)?;
 
-        panic!("Not implemented yet")
+        let access_flags = parser.read_u16()?;
+        let this_class = parser.read_u16()?;
+        let super_class = parser.read_u16()?;
+
+        let interfaces_count = parser.read_u16()?;
+        let interfaces = parser.read_u16_array(interfaces_count as usize)?;
+
+        let fields_count = parser.read_u16()?;
+        let fields = parser.read_u16_array(fields_count as usize)?;
+
+        let methods_count = parser.read_u16()?;
+        let methods = parser.read_u16_array(methods_count as usize)?;
+
+        let attributes_count = parser.read_u16()?;
+        let attributes = Self::read_attributes(&mut parser, attributes_count)?;
+
+        Ok(ClassFile {
+            minor_version,
+            major_version,
+            constant_pool_count,
+            constant_pool,
+            access_flags,
+            this_class,
+            super_class,
+            interfaces,
+            fields,
+            methods,
+            attributes,
+        })
+    }
+
+    /// Reads the constant pool from the class file.
+    fn read_constant_pool(
+        parser: &mut ClassFileParser,
+        constant_pool_count: u16,
+    ) -> Result<Box<[ConstantPoolInfo]>, Error> {
+        let mut constant_pool = Vec::with_capacity(constant_pool_count as usize);
+
+        for idx in 0..constant_pool_count {
+            if idx == 0 {
+                constant_pool.push(ConstantPoolInfo::Padding);
+            } else {
+                match constant_pool[(idx - 1) as usize] {
+                    ConstantPoolInfo::Long { .. } | ConstantPoolInfo::Double { .. } => {
+                        constant_pool.push(ConstantPoolInfo::Padding)
+                    }
+                    _ => constant_pool.push(ConstantPoolInfo::read_constant(parser)?),
+                }
+            }
+        }
+
+        Ok(constant_pool.into())
+    }
+
+    fn read_attributes(
+        parser: &mut ClassFileParser,
+        attributes_count: u16,
+    ) -> Result<Box<[AttributeInfo]>, Error> {
+        let mut attributes = Vec::with_capacity(attributes_count as usize);
+        for _ in 0..attributes_count {
+            let attribute_name_index = parser.read_u16()?;
+            let attribute_length = parser.read_u32()?;
+            let info = parser.read_u8_array(attribute_length as usize)?;
+            attributes.push(AttributeInfo {
+                attribute_name_index,
+                attribute_length,
+                info,
+            });
+        }
+        Ok(attributes.into())
     }
 }
 
+/// A constant pool entry in the class file format.
+///
+/// See [JVM Spec §4.4](https://docs.oracle.com/javase/specs/jvms/se25/html/jvms-4.html#jvms-4.4).
 pub enum ConstantPoolInfo {
+    Padding,
     Utf8 {
         length: u16,
         bytes: Box<[u8]>,
@@ -106,6 +200,7 @@ impl ConstantPoolInfo {
 
     pub const fn tag(&self) -> u8 {
         match self {
+            ConstantPoolInfo::Padding => panic!("Padding constant pool entry has no tag"),
             ConstantPoolInfo::Utf8 { .. } => Self::TAG_Utf8,
             ConstantPoolInfo::Integer { .. } => Self::TAG_Integer,
             ConstantPoolInfo::Float { .. } => Self::TAG_Float,
@@ -131,7 +226,7 @@ impl ConstantPoolInfo {
         Ok(match tag {
             Self::TAG_Utf8 => {
                 let length = parser.read_u16()?;
-                let bytes = parser.read_bytes(length as usize)?;
+                let bytes = parser.read_u8_array(length as usize)?;
                 ConstantPoolInfo::Utf8 { length, bytes }
             }
             Self::TAG_Integer => {
@@ -241,12 +336,24 @@ impl ConstantPoolInfo {
     }
 }
 
+/// An attribute in the class file format.
+///
+/// See [JVM Spec §4.7](https://docs.oracle.com/javase/specs/jvms/se25/html/jvms-4.html#jvms-4.7).
+pub struct AttributeInfo {
+    pub attribute_name_index: u16,
+    pub attribute_length: u32,
+    pub info: Box<[u8]>,
+}
+
 pub enum Error {
     UnexpectedEndOfFile,
+    InvalidMagicNumber(u32),
     UnknownConstantPoolInfo { tag: u8 },
 }
 
 /// A parser for the class file format.
+///
+/// All methods in this parser are big-endian.
 struct ClassFileParser<'a> {
     bytes: &'a [u8],
 }
@@ -282,11 +389,25 @@ impl<'a> ClassFileParser<'a> {
         Ok(u32::from_be_bytes(bytes))
     }
 
-    fn read_bytes(&mut self, size: usize) -> Result<Box<[u8]>, Error> {
+    fn read_u8_array(&mut self, size: usize) -> Result<Box<[u8]>, Error> {
         if self.bytes.len() >= size {
             let (head, tail) = self.bytes.split_at(size);
             self.bytes = tail;
             Ok(head.into())
+        } else {
+            Err(Error::UnexpectedEndOfFile)
+        }
+    }
+
+    fn read_u16_array(&mut self, size: usize) -> Result<Box<[u16]>, Error> {
+        let bytes_count = size * 2;
+        if self.bytes.len() >= bytes_count {
+            let (head, tail) = self.bytes.split_at(bytes_count);
+            self.bytes = tail;
+            Ok(head
+                .chunks_exact(2)
+                .map(|chunk| u16::from_le_bytes([chunk[0], chunk[1]]))
+                .collect())
         } else {
             Err(Error::UnexpectedEndOfFile)
         }
